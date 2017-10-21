9438b9802ec99b453e0b7d2f228c84595a03dc59
darkdrew <andrewleong14@gmail.com>
2017-10-21 20:31:14 +0800

    Added PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 // Local Imports
 // Grabbing the file for connection
@@ -78,10 +79,33 @@ app.delete('/todos/:id', (req,res) => {
       if(!todo) {
         return res.status(404).send();
       }
-      res.send(todo);
+      res.send({todo:todo});
     }).catch( (e) => {
       res.status(400).send();
     })
+})
+
+// UPDATE / PATCH
+app.patch('/todos/:id', (req,res) => {
+  const id = req.params.id;
+  // update to be stored
+  // _.pick takes an obj if exist
+  const body = _.pick(req.body, ['text', 'completed'])
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  if(_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then( (todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => res.status(400).send());
 })
 
 const port = process.env.PORT || 3000;
