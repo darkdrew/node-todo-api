bb6d206a97d6c18fdfa5b96cf196fd821d203b2a
darkdrew <andrewleong14@gmail.com>
2017-10-23 15:09:25 +0800

    added test for PATCH and DELETE

--- server/server.js
+++ server/server.js
@@ -73,13 +73,13 @@ app.get('/todos/:id', (req,res) => {
 app.delete('/todos/:id', (req,res) => {
   const id = req.params.id;
     if(!ObjectID.isValid(id)) {
-      return req.status(404).send();
+      return res.status(404).send();
     }
     Todo.findByIdAndRemove(id).then( (todo) => {
       if(!todo) {
         return res.status(404).send();
       }
-      res.send({todo:todo});
+      res.send({todo});
     }).catch( (e) => {
       res.status(400).send();
     })
